894928fb51ef1ac619a4b045f33d54e8ed02883b
Rick0335 <Rick0335@edu.eal.dk>
2019-09-10 13:51:18 +0200

    Sat navigations baren korrekt op, samt lavet de html pages der afspejler vores undersøgelse / forberedelse

---
+++ Pages/Login.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace _3_semester_projekt.Pages
+{
+    public class LoginModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
